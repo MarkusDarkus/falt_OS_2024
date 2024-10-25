d67552049ffe92d523a2edd16549820fd378be6a
Your Name <markshif1@.com>
2024-10-25 19:14:04 +0300

    change_t1

--- Task1/task1/task_01.c
+++ Task1/task1/task_01.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_LINE_LENGTH 256
-
-void execute_command_with_delay(const char *line) {
-    int delay;
-    char command[MAX_LINE_LENGTH];
-
-    // Читаем задержку и команду из строки
-    sscanf(line, "%d %[^\n]", &delay, command);
-
-    // Задержка перед выполнением команды
-    sleep(delay);
-
-    // Выполнение команды
-    system(command);
-}
+#define MAX_COMMAND_LENGTH 128
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -32,10 +21,29 @@ int main(int argc, char *argv[]) {
     }
 
     char line[MAX_LINE_LENGTH];
+    int delay;
+    char command[MAX_COMMAND_LENGTH];
+
     while (fgets(line, sizeof(line), file)) {
-        execute_command_with_delay(line);
+        if (sscanf(line, "%d %s", &delay, command) != 2) {
+            fprintf(stderr, "Ошибка формата строки: %s\n", line);
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Дочерний процесс
+            sleep(delay);
+            execlp(command, command, NULL);
+            perror("Ошибка выполнения команды");
+            exit(1);
+        }
     }
 
     fclose(file);
+
+    // Ожидание завершения всех дочерних процессов
+    while (wait(NULL) > 0);
+
     return 0;
 }
